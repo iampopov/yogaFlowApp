8d89bf08cd4fa44c110b8be9484958a75013fe93
monguis <jccnnetwork@gmail.com>
2020-03-02 14:20:37 -0700

    added my model work

---
+++ models/student.js
@@ -0,0 +1,62 @@
+const bcrypt = require("bcryptjs");
+
+module.exports = function (sequelize, DataTypes) {
+    const Students = sequelize.define("Students", {
+        student_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        login: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                is:["^(?=.{8,}$)(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9]).*$"],
+                len: [8]
+            }
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate:{
+                isEmail: true
+            }
+        },
+        favorites: {
+            type: DataTypes.STRING,
+            validate: {
+                len: [1]
+            }
+        }
+    })
+
+    Students.prototype.validPassword = function (password) {
+        return bcrypt.compareSync(password, this.password)
+    };
+
+    // Students.associate = (models) => {
+    //     Students.hasToMany(models.Groups, {
+    //       through: 'GroupUsers',
+    //       as: 'groups',
+    //       foreignKey: 'userId'
+    //     });
+    //   };
+
+    function encryptPasswordIfChanged(user) {
+        if (user.changed('password')) {
+            user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null)
+        }
+    }
+
+    Students.beforeCreate(encryptPasswordIfChanged);
+    Students.beforeUpdate(encryptPasswordIfChanged);
+    return Students
+}
