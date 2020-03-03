7b0bac94e1c7b4fa0b768ffd2afc7ad1a0069851
Dmboucher242 <58062008+Dmboucher242@users.noreply.github.com>
2020-03-03 14:04:57 -0700

    setting up routes to login and home page as well as adding content to the login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import Header from "./Pages/Home/components/Header";
 import Menu from "./Pages/Home/components/Menu";
-// import { BrowserRouter as Router, Route } from "react-router-dom";
-// import Card from "./Pages/Login/components/Card"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Card from "./Pages/Login/components/Card"
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="container">
-      <Header />
-      <Menu />
-      {/* <Card /> */}
-      {/* <Router> */}
-      {/* <Header /> */}
-      {/* <Route exact path='/' component={About} /> */}
-      {/* <Route exact path='/login' component={Card} /> */}
-      {/* <Route exact path='/resume' component={Resume} /> */}
-      {/* <Route exact path='/resume/contact' component={MyForm} /> */}
-      {/* <Footer /> */}
-    {/* </Router> */}
+
+
+
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Card />
+          </Route>
+
+          <Route exact path='/home'>
+            <Header />
+            <Menu />
+          </Route>
+
+
+
+          <Route exact path='/login' component={Card} />
+        </Switch>
+      </Router>
     </div>
   );
 }
