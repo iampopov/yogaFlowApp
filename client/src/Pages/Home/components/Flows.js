0c1d99b5d9419a664e030771a3e800d9d4cf6803
iampopov <ivantsar@gmail.com>
2020-03-04 11:37:35 -0700

    updated app.js for additional routes to flows and constructors

---
+++ client/src/Pages/Home/components/Flows.js
@@ -0,0 +1,16 @@
+import React from "react";
+//import { Card, CardBody, CardTitle, CardText, CardLink } from 'reactstrap';
+
+
+const Flows = props => {
+      return (
+
+              <>
+                <h1>this is Flows</h1>
+            </>
+
+     );
+  }
+
+
+export default Flows;
--- client/src/Pages/Home/components/Header.js
+++ client/src/Pages/Home/components/Header.js
@@ -1,15 +1,11 @@
 import React from "react";
-<<<<<<< HEAD:client/src/components/Header/index.js
-import { FooterWrapper } from "../../styles"
-=======
 import "../../../styles/Header.css"
->>>>>>> 25bef0a83dd327575ded5a128a37e5b610a92d89:client/src/Pages/Home/components/Header.js
 
 function Header() {
     return (
-   <FooterWrapper class="header">
+   <div class="header">
        <h1>Yogi</h1>
-   </FooterWrapper>
+   </div>
 
     )
 }
