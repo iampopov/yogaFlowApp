70c3c86a83a71a4eca6d8933ecc40dff392e0006
Teddy Luben <teddyluben@Teddys-MacBook-Pro.local>
2020-03-06 13:14:00 -0700

    component update

---
+++ client/src/Pages/Home/components/PlaceHolder.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function PlaceHolder(){
+    return(
+<div className="container">
+  <div className="card">
+    <div className="card body">
+      <img
+        alt=""
+        src="https://www.online-tech-tips.com/wp-content/uploads/2020/01/Windows-VLC-1.png"
+      />
+    </div>
+  </div>
+</div>
+    )}
+export default PlaceHolder;
---
+++ client/src/Pages/Home/components/Player.js
@@ -0,0 +1,69 @@
+import React from "react";
+
+function Player() {
+    return (
+        <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className="card">
+              <div className="card-body">
+                <h2>Name of Flow</h2>
+                <div className="card">
+                  <div className="card-body">
+                    <div className="row">
+                      <div className="col">
+                        <img
+                          src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fimages.shape.mdpcdn.com%2Fsites%2Fshape.com%2Ffiles%2Fstyles%2Fslide%2Fpublic%2F2._side-plank-420x420_0.jpg&f=1&nofb=1"
+                          alt=""/>
+                      </div>
+                      <div className="col-5">
+                        <p>Name of Pose</p>
+                        <p>Creator</p>
+                        <p>Timer Current Pose</p>
+                        <p>Timer Overall</p>
+                        <p>Teacher Notes</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br />
+        <div className="card">
+          <div className="card-body">
+            <h2 className="text-center">Upcoming Poses</h2>
+            <div className="row">
+              <div className="col">
+                <div className="card">
+                  <div className="card-body">
+                    <h4>Name of Pose</h4>
+                    <img
+                      className="centered"
+                      src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fimages.shape.mdpcdn.com%2Fsites%2Fshape.com%2Ffiles%2Fstyles%2Fslide%2Fpublic%2F2._side-plank-420x420_0.jpg&f=1&nofb=1"
+                      alt=""/>
+                    <p>Pose Duration</p>
+                  </div>
+                </div>
+              </div>
+              <div className="col">
+                <div className="card">
+                  <div className="card-body">
+                    <h4>Name of Pose</h4>
+                    <img
+                      className="centered"
+                      src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fimages.shape.mdpcdn.com%2Fsites%2Fshape.com%2Ffiles%2Fstyles%2Fslide%2Fpublic%2F2._side-plank-420x420_0.jpg&f=1&nofb=1"
+                    alt=""/> 
+                    <p>Pose Duration</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> 
+    )
+}
+
+export default Player;
