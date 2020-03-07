a515f30169c045fc531f8b725e6f627942ab338c
iampopov <ivantsar@gmail.com>
2020-03-06 17:02:50 -0700

    wired up a landing page for constructor

--- client/src/Pages/Constructor/components/Landing.js
+++ client/src/Pages/Constructor/components/Landing.js
@@ -1,30 +1,92 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Jumbotron, Button, FormGroup, Label, Input, FormText, CardImg, ListGroupItem, Row } from 'reactstrap';
 // import "../../../styles/"
 import Constructor from "./Constructor";
+import API from "../../../utils/API"
 
 function Landing() {
-    const [land, setLand] = useState({ checked: false, landed: false })
+    const [clicked, setClicked] = useState(false)
+    // const [land, setLand] = useState({ checked: false, landed: false })
+    const [flowName, setFlowName] = useState("");
+    const [teacher, setTeacher] = useState(
+        {"id": "",
+        "teacher_name": "",
+        "login": "",
+        "email": "",
+        "favorites": "",
+        "password": ""
+    });
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+    const [selectedTeacher, setSelectedTeacher] = useState();
 
-    const handleFormRender = (e, user) => {
-        e.preventDefault()
-        let temp = false;
-        if (user) {
-            temp = user;
-        }
-        setLand({
-            checked: true,
-            landed: temp
-        });
+    const toggle = () => setDropdownOpen(prevState => !prevState);
+
+    const handleFlowNameChange = (e) => {
+        setFlowName( e.target.value )
+    };
+    
+    useEffect(() => {
+        loadTeachers();
+    });
+
+    const loadTeachers = () => {
+        API.pullTeachers()
+        .then(res => setTeacher(res.data)) //setTeacher(res.data)
+        .catch(err => console.log(err))
+    };
+
+    const handleTeacherSelection = (e) => {
+        console.log(e.target.id);
+        setSelectedTeacher(e.target.id)
+    };
+
+    const handleRender = (e) => {
+        //console.log(e.target);
+        // console.log(clicked);
+        
+        // clicked = true;
+        // console.log(clicked);
+        setClicked(!clicked)
+        // e.preventDefault()
+        // let temp = false;
+        // if (clicked) {
+        //     temp = clicked;
+        // }
+        // setLand({
+        //     checked: true,
+        //     landed: temp
+        // });
     }
     return (
         <>
-        {/* <h1>You've landed</h1> */}
-        <Constructor />
-            {/* {land.checked ? <></> : <div handleFormRender={handleFormRender} />}
-            {land.checked && !land.landed ?  <SignUpForm />: land.checked && land.landed ? <SignInForm/>: <></> } */}
-
+        {clicked ? ( <Constructor />) : (
+        <>
+        <FormGroup>
+                <Label for="nameFlow">Name your flow:</Label>
+                <Input type="text" id="nameFlow" placeholder="flow name" onChange={handleFlowNameChange} value={flowName} />
+        </FormGroup>
+        <h1>Get the credit for this flow: </h1>
+        {teacher.length ? (
+            <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+            <DropdownToggle caret>
+                Select yourself!
+                </DropdownToggle>
+            <DropdownMenu onClick={handleTeacherSelection}>
+                {teacher.map(teacher => (
+                  <DropdownItem id={teacher.id} key={teacher.id}>{teacher.teacher_name}</DropdownItem>  
+                ))}
+            </DropdownMenu>
+            </Dropdown>
+        ) : ( 
+            <h3>No Results to Display</h3>
+        )}
+        <h1>Ready to do it? Let's </h1> <Button teacher_id={selectedTeacher} flow_name={flowName} onClick={handleRender}>create the flow</Button>
+        </>
+        )}
+        
         </>
     )
-}
+    }
+
 
 export default Landing;
